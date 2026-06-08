64626b93c6fabbd748888a55b3d578e01bd9ee2e
BonaventureMaina <bonaventuremaina946@gmail.com>
2026-06-08 13:04:41 +0300

    ci: deploy Flutter web to GitHub Pages

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -2,7 +2,7 @@ import 'package:flutter/foundation.dart';
 
 String get baseUrl {
   if (kIsWeb) {
-    return 'http://localhost:8000';
+    return 'https://smartbudget-api-m4q6.onrender.com';
   } else {
     // Android emulator
     return 'http://10.0.2.2:8000';
