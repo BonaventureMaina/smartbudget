5b836779c0e0ba31c3e41cfe74a7025c2fdc7d4e
BonaventureMaina <BonaventureMaina@users.noreply.github.com>
2026-06-09 09:09:46 +0000

    deploy: 1518b39bf2ed5a9889785b7a188ed8545dde581c

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "128174694" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3180765378" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
