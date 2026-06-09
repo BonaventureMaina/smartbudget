c2a43b80f055644e77f8ae66bcca4525100d81ce
BonaventureMaina <bonaventuremaina946@gmail.com>
2026-06-09 17:58:59 +0300

    feat: Android app with custom icon, name, and internet permission

--- frontend/lib/config.dart
+++ frontend/lib/config.dart
@@ -1,10 +1,1 @@
-import 'package:flutter/foundation.dart';
-
-String get baseUrl {
-  if (kIsWeb) {
-    return 'https://smartbudget-api-m4q6.onrender.com';
-  } else {
-    // Android emulator
-    return 'http://10.0.2.2:8000';
-  }
-}
+const String baseUrl = 'https://smartbudget-api-m4q6.onrender.com';
